11b9b46eeea27e246cb89dc5b408cce1e8ba3f18
vinycius monteiro <vinyciussilvamonteiro@gmail.com>
2020-03-04 23:57:45 -0300

    ajusta banco de dados

--- app/database.js
+++ app/database.js
@@ -2,9 +2,7 @@ const Sequelize = require('sequelize');
 const config = require('config');
 
 const sequelize = new Sequelize(
-  config.get('Customer.dbConfig.dbName'),
-  config.get('Customer.dbConfig.user'),
-  config.get('Customer.dbConfig.dbSecret'),
+  "mysql://be672d21e05dfc:3b3cb550@us-cdbr-iron-east-04.cleardb.net/heroku_c807af5398069ba?reconnect=true",
     {dialect: 'mysql', host: config.get('Customer.dbConfig.host')}
   )
 
